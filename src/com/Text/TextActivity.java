48e0027df40ccc89713d3844261a93b2d06bf697
Administrator <Administrator@20110219-1531>
2012-11-09 23:08:25 +0800

    Text

---
+++ src/com/Text/TextActivity.java
@@ -0,0 +1,13 @@
+package com.Text;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TextActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
